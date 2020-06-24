404f46b2bac08d6a1ea012b2bdb1d842cc71b764
tutajm <mtutaj@mcw.edu>
2020-06-24 15:49:21 -0500

    simplified shell script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # array-id-import-pipeline
-Import array ids with synergizer
+import AffyMetrix ids for rat, mouse, human and other species (if available) into ALIASES table.
+Synergizer java library is used by the project.
--- src/main/dist/import.sh
+++ src/main/dist/import.sh
@@ -4,11 +4,15 @@ APPDIR=/home/rgddata/pipelines/ArrayIdImport
 
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 if [ "$SERVER" == "REED" ]; then
-  EMAIL_LIST=RGD.Developers@mcw.edu
+  EMAIL_LIST=rgd.devops@mcw.edu
 else
   EMAIL_LIST=mtutaj@mcw.edu
 fi
 
-$APPDIR/_run.sh "$@"
+cd $APPDIR
+
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@" | tee run.log 2>&1
 
 mailx -s "[$SERVER] Array Id Import Pipeline Run " < $APPDIR/logs/summary.log $EMAIL_LIST
